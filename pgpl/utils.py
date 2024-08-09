81934dd73a2b2a2df6de150813c00db262bbe3fd
infstellar <98541813+infstellar@users.noreply.github.com>
2024-08-09 17:23:08 +0800

    fix

--- pgpl/utils.py
+++ pgpl/utils.py
@@ -365,6 +365,7 @@ def select_fastest_url(urls:typing.List[str]):
 
     input_str = str(urls)
     if not os.path.exists(f"{ROOT_PATH}\\cache\\url_speed_test.json"):
+        verify_path(f"{ROOT_PATH}\\cache")
         with open(f"{ROOT_PATH}\\cache\\url_speed_test.json", 'w', encoding='utf-8') as f:
             f.write('{}')
     x = load_json(f"{ROOT_PATH}\\cache\\url_speed_test.json")
